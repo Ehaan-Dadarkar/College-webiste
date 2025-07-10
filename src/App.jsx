510b10cb3615b819835a9eec6c84bb46447f92fb
Ehaan Dadarkar <ehaandadarkar2@gmail.com>
2025-07-11 02:03:13 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+<<<<<<< HEAD
 
 function App() {
 
+=======
+import { useEffect } from "react";
+
+function App() {
+
+  useEffect(() => {
+    const links = document.querySelectorAll(".nav-link"); // Select all nav links
+    const navbar = document.querySelector(".navbar-collapse"); // Select the navbar menu container
+
+    links.forEach(link => {
+      link.addEventListener("click", () => {
+        navbar.classList.remove("show"); // Force navbar to collapse (hide)
+      });
+    });
+  }, []);
+
+>>>>>>> b09ad21 (Initial commit)
   return (
     <>
       {/* Navbar */}
@@ -9,12 +27,21 @@ function App() {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
+<<<<<<< HEAD
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item"><a className="nav-link poppins link-info" href="#about" onclick="scrollToSection('about'); return false;">About</a></li>
             <li className="nav-item"><a className="nav-link poppins link-info" href="#courses">Courses</a></li>
             <li className="nav-item"><a className="nav-link poppins link-info" href="#admission">Admission</a></li>
             <li className="nav-item"><a className="nav-link poppins link-info" href="#contact">Contact</a></li>
+=======
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav" >
+          <ul className="navbar-nav">
+            <li className="nav-item"><a className="nav-link poppins link-info" href="#about">About</a></li>
+            <li className="nav-item"><a className="nav-link poppins link-info" href="#courses" >Courses</a></li>
+            <li className="nav-item"><a className="nav-link poppins link-info" href="#admission" >Admission</a></li>
+            <li className="nav-item"><a className="nav-link poppins link-info" href="#contact" >Contact</a></li>
+>>>>>>> b09ad21 (Initial commit)
           </ul>
         </div>
       </nav>
@@ -83,7 +110,11 @@ function App() {
             <div className="admission-step sans">1. Online Registration</div>
             <div className="admission-step sans">2. Document Verification</div>
             <div className="admission-step sans">3. Final Admission</div>
+<<<<<<< HEAD
             <button className="btn btn-warning mt-4">Apply Now</button>
+=======
+            <a href="https://www.aiarkp.ac.in" className="btn btn-warning mt-4" target="_blank">Apply Now</a>
+>>>>>>> b09ad21 (Initial commit)
           </div>
         </div>
       </section>
@@ -95,7 +126,11 @@ function App() {
           <div className="row justify-content-center">
             <div className="col-12 d-flex align-items-center gap-3 m-2">
               <img src="/icons8-address-24.png" alt="Address Icon" style={{ width: 30 }} />
+<<<<<<< HEAD
               <p className="sans mb-0">Jane Doe<br />1234 Elm Street, Apt 5A<br />Brooklyn, NY 11201, USA</p>
+=======
+              <a href="https://www.google.com/maps/place/Anjuman-I-Islam's+Abdul+Razzaq+Kalsekar+Polytechnic/@19.0004128,73.1024011,17z/data=!3m1!4b1!4m6!3m5!1s0x3be7b94f8441e107:0x51bb2a9aea66d07a!8m2!3d19.0004128!4d73.104976!16s%2Fg%2F11f33hd234?entry=ttu&g_ep=EgoyMDI1MDcwNy4wIKXMDSoASAFQAw%3D%3D" target="_blank" className="text-white text-decoration-none link-info sans ">AIARKP Institute<br />Karnala Knowledge Park,<br />Panvel, Navi Mumbai – 410206</a>
+>>>>>>> b09ad21 (Initial commit)
             </div>
             <div className="col-12 d-flex align-items-center gap-3 m-2">
               <img src="/icons8-phone-30.png" alt="Phone Icon" style={{ width: 30 }} />
